ce2528aaa129827bf7676a3e9417d6104a6b7e1d
Jamje Kim <jamje.kim@bespinglobal.com>
2019-01-07 15:59:37 +0900

    Unit test add

--- src/test/java/com/nalbam/sample/demo/HomeControllerTests.java
+++ src/test/java/com/nalbam/sample/demo/HomeControllerTests.java
@@ -1,9 +1,20 @@
 package com.nalbam.sample.demo;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RunWith(MockitoJUnitRunner.class)
 public class HomeControllerTests {
 
+    @Test
+    public void contextLoads() {
+        log.info("HomeControllerTests");
+
+    	log.debug("## contextLoads ##");
+    	
+    }
 }
